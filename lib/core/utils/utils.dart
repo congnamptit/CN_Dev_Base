cd10be6eb109ba1c0732f0e6a8e5b5d937e18b4e
namnc <namnc@skydart.app>
2022-06-16 15:23:49 +0700

    add utils.dart base

---
+++ lib/core/utils/utils.dart
@@ -0,0 +1,16 @@
+///
+/// A collection of useful functions
+///
+library utils;
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:intl/intl.dart';
+
+part 'extensions/list.dart';
+part 'responsive/responsive.dart';
+part 'responsive/app_responsive.dart';
+part 'debouncer.dart';
+part 'convert_number.dart';
