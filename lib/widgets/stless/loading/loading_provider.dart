457006796cc18364afe995c9acfe1dc535ed7c6f
namnc <namnc@skydart.app>
2022-06-17 10:54:39 +0700

    refactor

--- lib/widgets/stless/loading/loading_provider.dart
+++ lib/widgets/stless/loading/loading_provider.dart
@@ -1,7 +1,8 @@
 import 'package:cn_base/router/router.dart';
-import 'package:cn_base/widgets/loading/indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'indicator.dart';
 
 final loadingProvider = Provider(
   (ref) => LoadingProvider(ref.read),
